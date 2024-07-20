94b8164aa85f09b56184fe98318d505fd097dee5
Douglas2oo <1832641822@qq.com>
2024-07-20 21:34:33 +0800

    simple information render

--- miniprogram/pages/chargingDetail/chargingDetail.js
+++ miniprogram/pages/chargingDetail/chargingDetail.js
@@ -5,24 +5,38 @@ Page({
   stationInfo: null
   },
   onLoad: function(options) {
-  const stationId = options.id; // 获取传递过来的参数
-  this.loadStationInfo(stationId);
+    const stationId = parseInt(options.id, 10); // 获取传递过来的参数
+    this.loadStationInfo(stationId);
   },
+
   loadStationInfo: function(stationId) {
-  // 模拟从后端获取数据
-  // 实际应用中，你需要发送网络请求到你的服务器
-  // 这里使用假数据来模拟
-  const mockData = {
-    id: stationId,
-    name: '亿电邦科充电站',
-    location: '云升科学园',
-    price: '0.85元/度',
-    distance: '1.4km',
-    averageTime: '2小时',
-    costPerHour: '0.17元/分钟'
-  };
-  this.setData({
-    stationInfo: mockData
+    const app =  getApp();
+    const mockData = app.globalData.originalStationData.find(station => station.id === stationId);
+    console.log(mockData);
+    this.setData({
+      stationInfo: mockData
+    });
+  },
+
+  // 点击导航按钮---------------------------------------------------------
+  onNavigateTap: function() {
+    console.log('触发导航');
+    // wx.navigateTo({
+    //   url: '/pages/chargingMap/chargingMap'
+    // });
+  },
+  //---------------------------------------------------------------------
+  // 点击收藏按钮---------------------------------------------------------
+  addFavorite: function() {
+    console.log('添加收藏');
+  },
+  //---------------------------------------------------------------------
+  // 扫码充电------------------------------------------------------------
+  onScan: function() {
+    console.log('扫码充电');
+    wx.navigateTo({
+      url: '/pages/scan/scan',
   });
-  }
+  },
+  //---------------------------------------------------------------------
 });
